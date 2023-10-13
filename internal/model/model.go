e89e5f4135366c21a52ddcd83529cd3db44dd5f3
szmulinho <samik1997@gmail.com>
2023-10-13 12:45:46 +0200

    moving db

--- internal/model/model.go
+++ internal/model/model.go
@@ -17,13 +17,6 @@ var Orders []Order
 
 var NewOrder Order
 
-type User struct {
-	ID       int64  `gorm:"primaryKey;autoIncrement"`
-	Login    string `gorm:"unique" json:"login"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
-}
-
 type Exception struct {
 	Message string `json:"message"`
 }
